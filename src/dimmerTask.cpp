9ea9c6a7c693858a4fc173bd0c45424efeb67546
Cellie <CelliesProjects@users.noreply.github.com>
2024-10-21 15:46:31 +0200

    Added function to get milliseconds since midnight

--- src/dimmerTask.cpp
+++ src/dimmerTask.cpp
@@ -1,5 +1,26 @@
 #include "dimmerTask.hpp"
 
+static unsigned long long msSinceMidnight()
+{
+    // Time should already be synced through SNTP!
+    struct timeval now;
+    gettimeofday(&now, NULL);
+
+    struct tm currentTime;
+    localtime_r(&now.tv_sec, &currentTime);
+
+    int secondsSinceMidnight =
+        currentTime.tm_hour * 3600 +
+        currentTime.tm_min * 60 +
+        currentTime.tm_sec;
+
+    unsigned long long millisecondsSinceMidnight =
+        (unsigned long long)secondsSinceMidnight * 1000ULL +
+        now.tv_usec / 1000;
+
+    return millisecondsSinceMidnight;
+}
+
 void dimmerTask(void *parameter)
 {
     // https://docs.espressif.com/projects/arduino-esp32/en/latest/api/ledc.html
@@ -9,18 +30,20 @@ void dimmerTask(void *parameter)
     const uint8_t ledPin[] = {38, 39, 40, 41, 42};
 
     for (auto pin = 0; pin < sizeof(ledPin); pin++)
-        if (!ledcAttachChannel(ledPin[pin], freq, SOC_LEDC_TIMER_BIT_WIDTH, pin + 1)) // plus 1 because the tft backlight channel = 0
+        if (!ledcAttachChannel(ledPin[pin], freq, SOC_LEDC_TIMER_BIT_WIDTH, pin + 1)) // plus 1 because the tft backlight is channel 0
         {
             log_e("Error setting ledc channel %i. system halted", pin);
             while (1)
                 delay(1000);
         }
 
-    log_i("done");
+    constexpr const auto TICK_RATE_HZ = 1;
+    constexpr const TickType_t ticksToWait = pdTICKS_TO_MS(1000 / TICK_RATE_HZ);
+    static TickType_t xLastWakeTime = xTaskGetTickCount();
 
-    // task should run at 100Hz
     while (1)
     {
-        vTaskDelay(pdTICKS_TO_MS(100));
+        vTaskDelayUntil(&xLastWakeTime, ticksToWait);
+        log_i("%llu milliseconds since midnight", msSinceMidnight());
     }
 }
