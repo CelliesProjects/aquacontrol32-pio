2e8f72773413f8d834730f29ecf796cd42b01a4f
Cellie <CelliesProjects@users.noreply.github.com>
2024-12-27 12:23:16 +0100

    Cleanup

--- src/lcdTask.hpp
+++ src/lcdTask.hpp
@@ -5,7 +5,8 @@
 #include <LGFX_AUTODETECT.hpp>
 
 #include "lcdMessage.h"
-#include "fonts/DejaVu24-modded.h" /* contains percent sign and superscript 2*/
+#include "fonts/DejaVu24-modded.h" /* contains percent sign and a modified superscript 2 - to subscript*/
+                                   /* modded with https://tchapi.github.io/Adafruit-GFX-Font-Customiser/ */
 
 extern float currentPercentage[NUMBER_OF_CHANNELS];
 
