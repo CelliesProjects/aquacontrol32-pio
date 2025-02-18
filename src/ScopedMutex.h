7a6bc3ef1856d94f9da46482b2d2fb7ed594a8df
Cellie <CelliesProjects@users.noreply.github.com>
2025-02-18 23:19:59 +0100

    ScopedMutex: Delete copy constructor and assignment operator
    This commit prevents copying a ScopedMutex object. Copying a ScopedMutex would lead to multiple objects managing the same FreeRTOS semaphore, which is incorrect and can cause race conditions or deadlocks. The copy constructor and assignment operator are deleted to enforce single ownership of the lock and prevent this error at compile time.

--- src/ScopedMutex.h
+++ src/ScopedMutex.h
@@ -11,7 +11,11 @@ private:
     bool locked;
 
 public:
-    explicit ScopedMutex(SemaphoreHandle_t &m, TickType_t timeout = pdMS_TO_TICKS(1000)) : mutex(m), locked(xSemaphoreTake(mutex, timeout)) {}
+    explicit ScopedMutex(SemaphoreHandle_t &m, TickType_t timeout = pdMS_TO_TICKS(1000))
+        : mutex(m), locked(xSemaphoreTake(mutex, timeout)) {}
+
+    ScopedMutex(const ScopedMutex &) = delete;
+    ScopedMutex &operator=(const ScopedMutex &) = delete;
 
     ~ScopedMutex()
     {
